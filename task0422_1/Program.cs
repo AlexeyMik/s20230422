5b113dfc98d9fb90df1456ca507db0e844579e06
Alexey_mik <mikmav.mik@gmail.com>
2023-04-28 18:58:22 +0300

    условия задач - в ком к ним

--- task0422_1/Program.cs
+++ task0422_1/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Task 20230422_4 C sharp: вычислние суммы чётных чисел от нуля до заданного с консоли");
-// Попытка 
+// и попутно вывод всех четных чисел до заданного 
 Console.WriteLine("Введите целое число: ");
 int Number = Convert.ToInt32(Console.ReadLine());
 int i=0;
@@ -11,9 +11,16 @@ while (i<=Number)
     {
     acc=acc+i;
 // Трассировка для отладки
-//   Console.WriteLine("Для "+i+" остаток="+j+" Сумма равна "+acc);
+    Console.WriteLine($"{i} делится на 2, остаток={j}");
     };
     i++;
-}
-acc=acc+Number;
-Console.WriteLine("Сумма равна "+acc);
+};
+Console.WriteLine($"Сумма равна {acc}");
+// второй ваариант с циклом for
+acc=0;
+for (i=0; i<=Number; i=i+2)
+{
+    Console.WriteLine($"к {acc} прибавляем {i}");
+    acc=acc+i;
+};
+Console.WriteLine($"Второй вариант дает сумму {acc}");
---
+++ task0423_1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// нужно генерить случайные трех значные числ
+// из них убирать вторуюцифры и превращать в двузначные
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine($"трехзначное случайное число {number}" );
+int ic= number/100;
+int j= number % 10;
+int result = ic*10+j;
+System.Console.WriteLine($"результат {result}" );
